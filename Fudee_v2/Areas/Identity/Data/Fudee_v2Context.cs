d2dcc96cdefca672a660e8ee1f4fce1931ad1a86
Giovanna-89 <jarosinska.jd@gmail.com>
2022-12-13 18:08:18 +0100

    login

---
+++ Fudee_v2/Areas/Identity/Data/Fudee_v2Context.cs
@@ -0,0 +1,22 @@
+﻿using Fudee_v2.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fudee_v2.Data;
+
+public class Fudee_v2Context : IdentityDbContext<AppUser>
+{
+    public Fudee_v2Context(DbContextOptions<Fudee_v2Context> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
